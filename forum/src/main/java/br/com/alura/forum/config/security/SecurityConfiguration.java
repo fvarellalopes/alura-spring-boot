aca7404da645c07141bb5185282cce6835b40513
Fernando Varella Lopes <fernando.lopes@wssim.com.br>
2021-01-14 22:19:34 -0300

    Desenvolvimento dos cursos de Spring Boot da Alura

---
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	// controle de acesso de usuarios
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+
+	// permissões de urls
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()//
+				.antMatchers(HttpMethod.GET, "/topicos").permitAll()//
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+
+	// config de recursos estaticos
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+
+}
