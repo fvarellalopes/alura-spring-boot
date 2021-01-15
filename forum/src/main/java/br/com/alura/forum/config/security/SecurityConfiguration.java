af90abcc7e023ad08998791b7762a92ccb58de76
Fernando Varella Lopes <fernando.lopes@wssim.com.br>
2021-01-14 23:01:11 -0300

    Adicionando autenticação

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,14 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
 	// controle de acesso de usuarios
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 	// permissões de urls
@@ -22,7 +29,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()//
 				.antMatchers(HttpMethod.GET, "/topicos").permitAll()//
-				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()//
+				.antMatchers(HttpMethod.POST, "/auth").permitAll()//
+				.anyRequest().authenticated()//
+				.and().csrf().disable()//
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//
+				;
 	}
 
 	// config de recursos estaticos
